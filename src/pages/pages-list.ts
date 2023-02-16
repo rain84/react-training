f578fe65b2069ba8de22f3a563540c0b4930dfce
rain84 <rainy_sky@mail.ru>
2023-02-17 02:38:27 +0300

    feat: :sparkles: added list of app's pages

---
+++ src/pages/pages-list.ts
@@ -0,0 +1,9 @@
+export type TItem = {
+  name: string
+  to: string
+}
+
+export const PagesList: TItem[] = [
+  { name: 'Parent-Child rerender', to: 'parent-child-rerender' },
+  { name: 'GSAP', to: 'gsap' },
+]
