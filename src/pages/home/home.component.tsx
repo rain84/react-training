35d3ea6d8ddc8bc960803841a6503f5729fae155
rain84 <rainy_sky@mail.ru>
2023-02-16 07:55:38 +0300

    chore: :lipstick: updated ux

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export const Home = () => (
   <>
-    <p>HOME</p>
+    <p className="text-center text-red-400 text-xl">HOME</p>
     <Link to="parent-child-rerender">parent-child-rerender</Link>
   </>
 )
