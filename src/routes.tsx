eb4f5d748a9db7975da24d1b573a12cb184397db
rain84 <rainy_sky@mail.ru>
2023-02-16 20:15:14 +0300

    feat: :recycle: routes updated, added <Gsap> - route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,15 @@
 import { Route } from 'react-router-dom'
-import { Home, ParentChildRerender, Error404 } from 'pages'
+import { Home, ParentChildRerender, Error404, Gsap } from 'pages'
+import { AppLayout } from 'layouts'
 
 export const routes = (
-  <>
-    <Route path="/" element={<Home />} errorElement={<Error404 />}></Route>
+  <Route path="/" element={<AppLayout />}>
+    <Route index element={<Home />} />
     <Route
       path="/parent-child-rerender"
       element={<ParentChildRerender />}
     ></Route>
+    <Route path="/gsap" element={<Gsap />}></Route>
     <Route path="*" element={<Error404 />} errorElement={<Error404 />}></Route>
-  </>
+  </Route>
 )
