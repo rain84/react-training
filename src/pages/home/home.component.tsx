795727d73261b9e5395fe835c772d77e28648330
rain84 <rainy_sky@mail.ru>
2023-02-17 02:37:22 +0300

    refactor: :recycle: removed menu from <Home>

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,9 +1,5 @@
 import { Link } from 'react-router-dom'
 
 export const Home = () => (
-  <>
-    <p className="text-center text-red-400 text-xl">HOME</p>
-    <Link to="parent-child-rerender">parent-child-rerender</Link>
-    <Link to="gsap" className='text-green'>GSAP</Link>
-  </>
+  <p className="text-center text-red-400 text-xl">React Training</p>
 )
