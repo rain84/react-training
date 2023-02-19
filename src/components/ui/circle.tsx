d43e8bc2d390c7f76454c1e0d6c1000bb7bea66f
rain84 <rainy_sky@mail.ru>
2023-02-20 01:17:20 +0300

    feat: :sparkles: added <Circle>

---
+++ src/components/ui/circle.tsx
@@ -0,0 +1,13 @@
+import { HTMLAttributes, forwardRef } from 'react'
+
+export const Circle = forwardRef<HTMLDivElement, HTMLAttributes<Element>>(
+  ({ className, children, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={`rounded-full flex justify-center items-center h-12 w-12 p-2 ${className}`}
+      {...props}
+    >
+      <span>{children}</span>
+    </div>
+  )
+)
