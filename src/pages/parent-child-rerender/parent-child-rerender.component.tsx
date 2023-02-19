c970b9186985a2b0af9736297aa9349dd7b79ae1
rain84 <rainy_sky@mail.ru>
2023-02-19 19:43:48 +0300

    chore: :recycle: disabled parent-child rerendering

--- src/pages/parent-child-rerender/parent-child-rerender.component.tsx
+++ src/pages/parent-child-rerender/parent-child-rerender.component.tsx
@@ -5,14 +5,14 @@ export const ParentChildRerender = () => {
   const timer = 3000
   const [prop, setProp] = useState(0)
 
-  useEffect(() => {
-    setInterval(() => {
-      setProp(prop ? 0 : 1)
-      console.log('\n')
-    }, timer)
-  }, [prop])
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setProp(prop ? 0 : 1)
+  //     console.log('\n')
+  //   }, timer)
+  // }, [prop])
 
-  console.log('ParentChildRerender')
+  // console.log('ParentChildRerender')
 
   return (
     <section>
