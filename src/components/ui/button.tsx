87d987f147ad8381d3d5f0fd3635e10a4d57458e
rain84 <rainy_sky@mail.ru>
2023-02-19 18:13:12 +0300

    refactor: :recycle: refactored classes in <Button>

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,7 +4,7 @@ export const Button = forwardRef<HTMLDivElement, HTMLAttributes<Element>>(
   ({ className, children, ...props }, ref) => (
     <div
       ref={ref}
-      className={`h-12 w-12 rounded-md flex justify-center items-center bg-slate-300 p-2 ${className}`}
+      className={`rounded-md flex justify-center items-center h-12 w-12 p-2 ${className}`}
       {...props}
     >
       <span>{children}</span>
