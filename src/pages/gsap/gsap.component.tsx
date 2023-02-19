52abad28b07217478d8dc90592936efabfd25486
rain84 <rainy_sky@mail.ru>
2023-02-19 18:18:56 +0300

    feat: :lipstick: added <HoverMe> on Gsap-page, UI styles updated

--- src/pages/gsap/gsap.component.tsx
+++ src/pages/gsap/gsap.component.tsx
@@ -1,10 +1,13 @@
-import { GsapProvider, Rotate } from 'components/gsap'
+import { GsapProvider, Rotate, HoverMe } from 'components/gsap'
 
 export const Gsap = () => (
   <section>
     <p className="text-center text-red-600 uppercase">gsap</p>
     <GsapProvider>
-      <Rotate />
+      <div className="flex flex-row justify-start">
+        <Rotate className="mr-4" />
+        <HoverMe className="mr-4" />
+      </div>
     </GsapProvider>
   </section>
 )
