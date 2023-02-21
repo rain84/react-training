61434d871f5e5959d793323ed6fd6d5e68a6ef80
rain84 <rainy_sky@mail.ru>
2023-02-21 13:29:09 +0300

    test: :white_check_mark: added tests for useArray🪝

--- src/hooks/useArray.test.tsx
+++ src/hooks/useArray.test.tsx
@@ -5,12 +5,31 @@ import { useRef } from 'react'
 
 const Component = () => {
   const counter = useRef(0)
-  const [arr, { add, remove, clear }] = useArray()
+  const [arr, { add, remove, clear: clearArray }] = useArray()
+  const addNext3 = () => {
+    let c = 3
+    const arr = []
+    while (c--) arr.push(++counter.current)
+    add(...arr)
+  }
+  const removeNext3 = () => {
+    let c = 3
+    const arr = []
+    while (c--) arr.push(counter.current--)
+    remove(...arr)
+  }
+  const clear = () => {
+    counter.current = 0
+    clearArray()
+  }
+
   return (
     <>
       <button onClick={() => add(++counter.current)}>Add</button>
+      <button onClick={addNext3}>Add next 3</button>
       <button onClick={() => remove(counter.current--)}>Remove</button>
-      <button onClick={() => clear()}>Clear</button>
+      <button onClick={removeNext3}>Remove next 3</button>
+      <button onClick={clear}>Clear</button>
       <span data-testid="text">{arr.join('')}</span>
     </>
   )
@@ -20,7 +39,9 @@ test('useArray', async () => {
   render(<Component />)
 
   const add = screen.getByText('Add')
+  const addNext3 = screen.getByText('Add next 3')
   const remove = screen.getByText('Remove')
+  const removeNext3 = screen.getByText('Remove next 3')
   const clear = screen.getByText('Clear')
   const text = screen.getByTestId('text')
 
@@ -38,4 +59,13 @@ test('useArray', async () => {
 
   await userEvent.click(clear)
   expect(text.textContent).toBeFalsy()
+
+  await userEvent.click(addNext3)
+  await userEvent.click(addNext3)
+  expect(text.textContent).toBe('123456')
+
+  await userEvent.click(removeNext3)
+  expect(text.textContent).toBe('123')
+  await userEvent.click(removeNext3)
+  expect(text.textContent).toBe('')
 })
