16c3ed31a488c979569785f1e8ebdfbadbbd1bbc
rain84 <rainy_sky@mail.ru>
2023-02-21 17:24:45 +0300

    feat: :recycle: added debouncing for "onChange", fixed losted dependencies for "useGsap", added <Input.Number>

--- src/components/gsap/reusable-components.tsx
+++ src/components/gsap/reusable-components.tsx
@@ -1,8 +1,12 @@
-import { useContext, useRef } from 'react'
+import { ChangeEventHandler, useContext, useRef, useState } from 'react'
 import { GsapContext } from './context'
-import { Box } from 'ui'
+import { Box, Input } from 'ui'
+import debounce from 'lodash/debounce'
 
-export const ReusableComponents = ({ className }: IProps) => {
+const INITIAL_WIDTH = -80
+const INPUT_DELAY = 50
+
+export const ReusableComponents = () => {
   const { useGsap } = useContext(GsapContext)
   const box = {
     1: useRef(null),
@@ -10,18 +14,31 @@ export const ReusableComponents = ({ className }: IProps) => {
     3: useRef(null),
   }
 
+  //  update with "useDeferredValue"
+  const [width, setWidth] = useState(INITIAL_WIDTH)
+  const updateWidth: ChangeEventHandler<HTMLInputElement> = debounce(
+    (e) => setWidth(+e.target.value),
+    INPUT_DELAY
+  )
+
   //  prettier-ignore
   useGsap((gsap) => {
+    gsap.globalTimeline.kill()
     gsap.to(box[1].current, { rotation: 360, repeat: -1, repeatDelay: 1, yoyo: true })
-    gsap.to(box[3].current, { x: -80, repeat: -1, repeatDelay: 1, yoyo: true })
+    gsap.to(box[3].current, { x: width, repeat: -1, repeatDelay: 1, yoyo: true })
     gsap.set(box[2].current, { backgroundColor: 'red' })
-  })
+  }, [width])
 
   return (
     <div className="flex flex-col justify-between space-y-4">
       <Box ref={box[1]} className="bg-green-600">
         Box
       </Box>
+      <Input.Number
+        defaultValue={width}
+        onChange={updateWidth}
+        className="w-20"
+      />
       <Box ref={box[2]} className="bg-green-600">
         <span className="inline-block font-bold scale-50">
           Don't Animate Me
