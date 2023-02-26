79562731ae734595288defd6c608208cf5e6ba2a
rain84 <rainy_sky@mail.ru>
2023-02-26 21:05:04 +0300

    feat: :sparkles: added Play / Pause for <Rotate>

--- src/components/gsap/rotate.tsx
+++ src/components/gsap/rotate.tsx
@@ -1,18 +1,47 @@
 import { useContext, useRef } from 'react'
 import { GsapContext } from './context'
-import { Box } from 'ui'
+import { Box, ButtonPlay } from 'ui'
+import { useToggle } from 'hooks'
 
 export const Rotate = ({ className }: IProps) => {
   const { useGsap } = useContext(GsapContext)
-  const box = useRef(null)
+  const [isPlayed, toggle] = useToggle(true)
+  const box1 = useRef(null)
+  const box2 = useRef(null)
+  const box3 = useRef(null)
+  const tween = useRef<gsap.core.Tween>()
 
-  useGsap((gsap) => {
-    gsap.to(box.current, { rotation: '+360' })
-  })
+  useGsap(
+    (gsap) => {
+      tween.current = gsap.to([box1.current, box2.current, box3.current], {
+        rotation: '+360',
+        repeat: isPlayed ? -1 : 0,
+        duration: 1,
+        stagger: 0.5,
+        repeatDelay: 1,
+        ease: 'back.in',
+      })
+      if (!isPlayed) tween.current.kill()
+    },
+    [isPlayed]
+  )
 
   return (
-    <Box ref={box} className={`bg-orange-400 ${className}`}>
-      Hello
-    </Box>
+    <section className="flex flex-col space-y-4">
+      <ButtonPlay
+        onClick={toggle}
+        className="text-center"
+        isPlayed={isPlayed}
+      />
+      <Box ref={box1} className={`bg-orange-400 ${className}`}>
+        Hello 1
+      </Box>
+      <Box ref={box2} className={`bg-orange-400 ${className}`}>
+        Hello 1
+      </Box>
+      <Box ref={box3} className={`bg-orange-400 ${className}`}>
+        Hello 1
+      </Box>
+    </section>
   )
 }
