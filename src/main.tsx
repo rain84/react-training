a8358ffc8e6f676a122c12dd3c228586a61e02e7
rain84 <rainy_sky@mail.ru>
2023-02-26 19:02:28 +0300

    feat: :lipstick: added sass styles for the whole app

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom'
 import { routes } from './routes'
 import './index.css'
+import './index.sass'
 
 const router = createBrowserRouter(createRoutesFromElements(routes))
 
