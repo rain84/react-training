5b3660d9ec69c54cf01ff4fd465eb46bd4df09f0
rain84 <rainy_sky@mail.ru>
2023-02-26 20:05:16 +0300

    feat: :label: added <ButtonPlay>, updated <Button>

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,12 +1,27 @@
 import { HTMLAttributes, forwardRef } from 'react'
+import { PlayIcon } from '@radix-ui/react-icons'
 
-export const Button = forwardRef<HTMLDivElement, HTMLAttributes<Element>>(
-  ({ className, onClick, children, ...props }, ref) => (
-    <button
-      onClick={onClick}
-      className="p-2 border-2 rounded-md hover:bg-yellow-200"
-    >
-      {children}
-    </button>
-  )
-)
+export const Button = forwardRef<
+  HTMLButtonElement,
+  HTMLAttributes<HTMLButtonElement>
+>(({ className, onClick, children, ...props }, ref) => (
+  <button
+    {...props}
+    ref={ref}
+    onClick={onClick}
+    className="p-2 border-2 rounded-md hover:bg-yellow-200"
+  >
+    {children}
+  </button>
+))
+
+export const ButtonPlay = forwardRef<
+  HTMLButtonElement,
+  HTMLAttributes<HTMLButtonElement> & Partial<{ isPlay: boolean }>
+>(({ className, isPlay, children, ...props }, ref) => (
+  <Button {...props} className="text-center" ref={ref}>
+    <PlayIcon
+      className={`stroke-${isPlay ? 'green' : 'red'}-500 stroke-1 inline`}
+    />
+  </Button>
+))
