a234a727edc27fb7fa3495a97f30fffb4aa8512c
rain84 <rainy_sky@mail.ru>
2023-02-26 20:36:58 +0300

    feat: :alembic: investigations of re-rendering and props spreading

--- src/components/rerendering/children.tsx
+++ src/components/rerendering/children.tsx
@@ -3,15 +3,16 @@ import { getData } from 'utils'
 import { Subchildren } from '.'
 
 const data = getData()
+const { log } = console
 
 export const Children = memo(
   ({ prop, name }: { prop?: number; name: string }) => {
-    console.log(`${name}-Children`)
+    log(`${name}-Children`)
 
     return (
       <>
         {data.map((val) => {
-          console.log(`${name}-Children-list`)
+          log(`${name}-Children-list`)
           return <p key={val + prop}>{val}</p>
         })}
         <Subchildren name={`${name}-Children`} />
@@ -19,5 +20,16 @@ export const Children = memo(
     )
   }
 )
-
 Children.displayName = 'Children'
+
+export const ChildrenWithSpreadingProps = memo(({ ...props }) => {
+  log('children_with_spreaded_props')
+  return <p {...props}>children_with_spreaded_props</p>
+})
+Children.displayName = 'ChildrenWithoutSpreadingProps'
+
+export const ChildrenWithoutSpreadingProps = memo(() => {
+  log('children_without_spreaded_props')
+  return <p>children_without_spreaded_props</p>
+})
+Children.displayName = 'ChildrenWithoutSpreadingProps'
--- src/pages/rerendering/parent-child-rerender.page.tsx
+++ src/pages/rerendering/parent-child-rerender.page.tsx
@@ -1,24 +1,34 @@
-import { useEffect } from 'react'
-import { Parent1, Parent2, List, Subchildren } from 'components'
+import { useEffect, useRef } from 'react'
+import {
+  Parent1,
+  Parent2,
+  List,
+  Subchildren,
+  ChildrenWithSpreadingProps,
+  ChildrenWithoutSpreadingProps,
+} from 'components'
 import { useToggle } from 'hooks'
 
 export const RerenderingPage = () => {
   const timer = 3000
   const [prop, toggle] = useToggle()
+  const counter = useRef(0)
 
   useEffect(() => {
     setInterval(toggle, timer)
   }, [prop])
 
-  console.log('Component RerenderingPage')
+  console.log('Component RerenderingPage', counter.current++)
 
   return (
-    <section>
-      <Parent1 prop={prop} name={'App'} list={<List name="App-Parent1" />}>
+    <section data-prop={prop}>
+      {/* <Parent1 prop={prop} name={'App'} list={<List name="App-Parent1" />}>
         <Subchildren name="App-Parent1" />
       </Parent1>
       <br />
-      <Parent2 name={'App'} />
+      <Parent2 name={'App'} /> */}
+      <ChildrenWithSpreadingProps data-prop="prop" />
+      <ChildrenWithoutSpreadingProps data-prop="prop" />
     </section>
   )
 }
