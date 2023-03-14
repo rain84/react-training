5e1cbb6e986d4a35354c34681b87d67f2e0d82b7
rain84 <rainy_sky@mail.ru>
2023-03-14 19:34:45 +0300

    fix: :bug: fixed bug with a permanent creation of a new set-function

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 export const useToggle = (init: boolean): [boolean, () => void] => {
   const [state, setState] = useState(init)
-  return [state, () => setState((state) => !state)]
+  const toggle = useCallback(() => setState((state) => !state), [])
+  return [state, toggle]
 }
