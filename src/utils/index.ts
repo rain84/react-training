7b87a006aab853db8dfafa10b6d88f20f04dfd47
rain84 <rainy_sky@mail.ru>
2023-03-14 19:52:22 +0300

    feat: :sparkles: added withLog - util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,8 @@
 export const getData = () => [...'ab'].map((val) => val.repeat(10))
+export const withLog =
+  <T extends Function>(str: string) =>
+  (f?: T) =>
+    function (this: any, ...args: any) {
+      console.log(str)
+      return f?.apply(this, args)
+    }
