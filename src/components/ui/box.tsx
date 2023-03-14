c2b0e722674ec78e8585b87a564c9fb7c68bb779
rain84 <rainy_sky@mail.ru>
2023-03-14 14:33:45 +0300

    refactor: :recycle: added clsx

--- src/components/ui/box.tsx
+++ src/components/ui/box.tsx
@@ -1,10 +1,14 @@
+import clsx from 'clsx'
 import { HTMLAttributes, forwardRef } from 'react'
 
 export const Box = forwardRef<HTMLDivElement, HTMLAttributes<Element>>(
   ({ className, children, ...props }, ref) => (
     <div
       ref={ref}
-      className={`rounded-md flex justify-center items-center h-12 w-12 p-2 ${className}`}
+      className={clsx(
+        'rounded-md flex justify-center items-center h-12 w-12 p-2',
+        className
+      )}
       {...props}
     >
       {children}
--- src/components/ui/circle.tsx
+++ src/components/ui/circle.tsx
@@ -1,10 +1,14 @@
 import { HTMLAttributes, forwardRef } from 'react'
+import { clsx } from 'clsx'
 
 export const Circle = forwardRef<HTMLDivElement, HTMLAttributes<Element>>(
   ({ className, children, ...props }, ref) => (
     <div
       ref={ref}
-      className={`rounded-full flex justify-center items-center h-12 w-12 p-2 ${className}`}
+      className={clsx(
+        'rounded-full flex justify-center items-center h-12 w-12 p-2',
+        className
+      )}
       {...props}
     >
       <span>{children}</span>
