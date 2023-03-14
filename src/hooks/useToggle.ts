04db7e5868ac9801d7d196d62094edc1c401e13a
rain84 <rainy_sky@mail.ru>
2023-03-14 18:56:07 +0300

    refactor: :recycle: updated initialization

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-export const useToggle = (init = false): [boolean, () => void] => {
+export const useToggle = (init: boolean): [boolean, () => void] => {
   const [state, setState] = useState(init)
   return [state, () => setState((state) => !state)]
 }
