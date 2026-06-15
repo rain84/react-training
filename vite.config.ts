31b8901ed8ec854e855ab8110bf422e021438ec2
rain84 <rainy_sky@mail.ru>
2026-06-15 14:23:10 +0300

    Updated configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import { resolveAlias } from './utils'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     ...(await resolveAlias()),
   },
