788af6af748ddc13b585febe2b1be2e0aff41a67
rain84 <rainy_sky@mail.ru>
2026-08-17 12:43:15 +0300

    chore(config): update ts and vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { resolveAlias } from './utils'
+import { resolveAlias } from './utils.ts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
