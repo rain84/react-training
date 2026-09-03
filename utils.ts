6a0f990611c7687d4b3ed6cad98cf4f426cd9001
rain84 <rainy_sky@mail.ru>
2026-09-03 16:29:53 +0300

    style: apply biome formatting to root configs

--- utils.ts
+++ utils.ts
@@ -3,40 +3,40 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 
 type Entry = {
-  name: string
-  path: string
+	name: string
+	path: string
 }
 
 export const fileDirName = () => {
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = path.dirname(__filename)
+	const __filename = fileURLToPath(import.meta.url)
+	const __dirname = path.dirname(__filename)
 
-  return { __dirname, __filename }
+	return { __dirname, __filename }
 }
 
 // TODO: extract into separate 'vite-resolve-alias-plugin'
 export const resolveAlias = async () => {
-  const { __dirname } = fileDirName()
-  const src = path.resolve(__dirname, './src')
-  const entries = await fs.readdir(src)
-  const resources = (
-    await Promise.all(
-      entries.map((name) => {
-        const p = path.resolve(src, name)
-        return new Promise<Entry>(async (res, rej) => {
-          try {
-            res({
-              name,
-              path: p,
-            })
-          } catch (error) {
-            rej(error)
-          }
-        })
-      })
-    )
-  ).map(({ name, path }) => [name, path])
+	const { __dirname } = fileDirName()
+	const src = path.resolve(__dirname, './src')
+	const entries = await fs.readdir(src)
+	const resources = (
+		await Promise.all(
+			entries.map((name) => {
+				const p = path.resolve(src, name)
+				return new Promise<Entry>(async (res, rej) => {
+					try {
+						res({
+							name,
+							path: p,
+						})
+					} catch (error) {
+						rej(error)
+					}
+				})
+			}),
+		)
+	).map(({ name, path }) => [name, path])
 
-  const alias = Object.fromEntries(resources)
-  return { alias }
+	const alias = Object.fromEntries(resources)
+	return { alias }
 }
